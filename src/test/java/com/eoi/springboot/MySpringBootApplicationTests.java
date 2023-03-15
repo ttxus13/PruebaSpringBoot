1c45d0738ccff5f23d283c6aa61c5ce0b22e7d56
Ttxus <ttxus13@gmail.com>
2023-03-15 12:51:18 +0100

    PRB-1 renombrarr y reorganizar

--- src/test/java/com/eoi/springboot/MySpringBootApplicationTests.java
+++ src/test/java/com/eoi/springboot/MySpringBootApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.eoi.springboot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class MySpringBootApplicationTests {
 
 	@Test
 	void contextLoads() {
